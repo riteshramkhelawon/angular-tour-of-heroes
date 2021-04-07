5f151dcdfa62f7400554c6966dd788e67d481440
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2021-04-07 13:42:09 +0100

    feat: added message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service'
+import { MessageService } from '../message.service'
 
 @Component({
   selector: 'app-heroes',
@@ -16,10 +17,12 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, 
+    private messageService: MessageService) { }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero ${hero.name}`)
   }
   
   getHeroes(): void {
